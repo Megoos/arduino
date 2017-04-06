1aa00cc6a29c75f36b9bce5da98fe311697cf239
Megoos <meguskov@gmail.com>
2017-04-06 16:52:28 +0500

    fix read data pult

--- Baggi Car/test_pult/test_pult.ino
+++ Baggi Car/test_pult/test_pult.ino
@@ -1,55 +1,32 @@
-int PinOut[4] {5, 4, 3, 2}; // пины выходы
+int ch1; // Here's where we'll keep our channel values
+int ch2;
+int ch3;
 
-int PinIn[4] {9, 8, 7, 6}; // пины входа
-int val = 0;
-const char value[4][4]
-  
-{ {'1', '4', '7', '*'},
-  {'2', '5', '8', '0' },
-  {'3', '6', '9', '#'},
-  {'A', 'B', 'C', 'D'}
-};
-// двойной массив, обозначающий кнопку
- 
-int b = 0; // переменная, куда кладется число из массива(номер кнопки)
- 
-void setup()
-{
-  pinMode (2, OUTPUT); // инициализируем порты на выход (подают нули на столбцы)
-  pinMode (3, OUTPUT);
-  pinMode (4, OUTPUT);
-  pinMode (5, OUTPUT);
- 
-  pinMode (6, INPUT); // инициализируем порты на вход с подтяжкой к плюсу (принимают нули на строках)
-  digitalWrite(6, HIGH);
-  pinMode (7, INPUT);
-  digitalWrite(7, HIGH);
-  pinMode (8, INPUT);
-  digitalWrite(8, HIGH);
-  pinMode (9, INPUT);
-  digitalWrite(9, HIGH);
- 
-  Serial.begin(9600); // открываем Serial порт
+void setup() {
+
+  pinMode(5, INPUT); // Set our input pins as such
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+
+  Serial.begin(9600); // Pour a bowl of Serial
+
 }
- 
-void matrix () // создаем функцию для чтения кнопок
-{
-  for (int i = 1; i <= 4; i++) // цикл, передающий 0 по всем столбцам
-  {
-    digitalWrite(PinOut[i - 1], LOW); // если i меньше 4 , то отправляем 0 на ножку
-    for (int j = 1; j <= 4; j++) // цикл, принимающих 0 по строкам
-    {
-      if (digitalRead(PinIn[j - 1]) == LOW) // если один из указанных портов входа равен 0, то..
-      {
-        Serial.println( value[i - 1][j - 1]); // то b равно значению из двойного массива
-        delay(175);
-      }
-    }
-    digitalWrite(PinOut[i - 1], HIGH); // подаём обратно высокий уровень
-  }
-}
- 
-void loop()
-{
-  matrix(); // используем функцию опроса матричной клавиатуры 
+
+void loop() {
+
+  ch1 = pulseIn(5, HIGH, 25000); // Read the pulse width of 
+  ch2 = pulseIn(6, HIGH, 25000); // each channel
+  ch3 = pulseIn(7, HIGH, 25000);
+
+  Serial.print("Channel 1:"); // Print the value of 
+  Serial.println(ch1);        // each channel
+
+  Serial.print("Channel 2:");
+  Serial.println(ch2);
+
+  Serial.print("Channel 3:");
+  Serial.println(ch3);
+
+  delay(100); // I put this here just to make the terminal 
+              // window happier
 }
